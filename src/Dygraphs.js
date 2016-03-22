51f8b02f3205eb0fef585d9e908204fe998357e9
Alex Gryzlov <alexander.gryzlov@adform.com>
2016-03-22 20:04:07 +0200

    fix ffi

--- src/Dygraphs.js
+++ src/Dygraphs.js
@@ -2,6 +2,6 @@
 "use strict";
 
 // module DyGraphs 
-exports.unsafeNullToMaybeImpl(just, nothing, val) {
+exports.unsafeNullToMaybeImpl = function(just, nothing, val) {
   return val === null ? nothing : just(val);
 }
